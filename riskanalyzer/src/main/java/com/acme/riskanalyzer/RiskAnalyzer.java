6892651e0730f4a9264661c2606007c458fecec5
Zsolt Sandor <zsolt.sandor@gmail.com>
2022-05-18 07:18:43 +0200

    Support for maven assembly with some code cleanup
    Added support for maven assembly
    
    Code cleanup, renamed assembly file

--- riskanalyzer/src/main/java/com/acme/riskanalyzer/RiskAnalyzer.java
+++ riskanalyzer/src/main/java/com/acme/riskanalyzer/RiskAnalyzer.java
@@ -3,26 +3,20 @@ package com.acme.riskanalyzer;
 import com.nomagic.magicdraw.core.Application;
 import com.nomagic.magicdraw.plugins.Plugin;
 
-public class RiskAnalyzer extends Plugin
-{
-	public static boolean initialized;
-	
+public class RiskAnalyzer extends Plugin {
+
 	@Override
-	public void init()
-	{
-		initialized = true;
+	public void init() {
 		Application.getInstance().getGUILog().showMessage("RiskAnalyzer initialized.");
 	}
 
 	@Override
-	public boolean close()
-	{
+	public boolean close() {
 		return true;
 	}
 
 	@Override
-	public boolean isSupported()
-	{
+	public boolean isSupported() {
 		return true;
 	}
 }
