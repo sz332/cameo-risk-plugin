b0844bc689fbcfb77a0b218418c8a651c1b42d24
Zsolt Sandor <zsolt.sandor@gmail.com>
2022-05-22 21:20:10 +0200

    getting values from tags

--- riskanalyzer/src/main/java/com/acme/riskanalyzer/init/RiskAction.java
+++ riskanalyzer/src/main/java/com/acme/riskanalyzer/init/RiskAction.java
@@ -6,6 +6,11 @@ import com.nomagic.magicdraw.ui.browser.Tree;
 import com.nomagic.magicdraw.ui.browser.actions.DefaultBrowserAction;
 import com.nomagic.magicdraw.ui.dialogs.MDDialogParentProvider;
 
+import com.nomagic.magicdraw.core.*;
+import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
+import com.nomagic.uml2.ext.jmi.helpers.TagsHelper;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
+
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,14 +38,48 @@ public class RiskAction extends DefaultBrowserAction {
 
     private List<Risk> findRisks(Tree tree) {
 
+        var project = Application.getInstance().getProject();
+        var profile = StereotypesHelper.getProfile(project, "Safety");
+        var stereotype = StereotypesHelper.getStereotype(project, "Risk", profile);
+
         var risks = new ArrayList<Risk>();
 
-        risks.add(new Risk("", "Id-l-5-c-1", 5, 1));
-        risks.add(new Risk("", "Id-l-5-c-5", 5, 5));
-        risks.add(new Risk("", "Id-l-1-c-1", 1, 1));
-        risks.add(new Risk("", "Id-l-1-c-5", 1, 5));
-        risks.add(new Risk("", "Id-l-3-c-2", 3, 2));
-        risks.add(new Risk("", "Id-l-4-c-1", 4, 1));
+        System.err.println("project = " + project);
+        System.err.println("profile = " + profile);
+        System.err.println("stereotype = " + stereotype);
+
+        if (project != null && profile != null && stereotype != null){
+            var myPackage = project.getElementByID("_2021x_2_62c021d_1653239356108_45350_2833");
+
+            if (myPackage instanceof Package){
+                Package pkg = (Package) myPackage;
+
+                System.err.println("Found package");
+
+                for (var element : pkg.getOwnedElement()){
+                    if (StereotypesHelper.hasStereotype(element, stereotype)){
+                        var name = TagsHelper.getStereotypePropertyFirst(element, stereotype, "riskName");
+                        var id = TagsHelper.getStereotypePropertyFirst(element, stereotype, "riskID");
+                        var likelihood = TagsHelper.getStereotypePropertyFirst(element, stereotype, "likelihood");
+                        var maxConsequence = element.refGetValue("maxConsequence");
+
+                        System.err.println("human name = " + element.getHumanName());
+                        System.err.println("name =" + name);
+                        System.err.println("id =" + id);
+                        System.err.println("likelihood = " + likelihood);
+                        System.err.println("maxConsequence = " + maxConsequence);
+                    }
+                }
+            }
+
+        } else {
+            risks.add(new Risk("", "Id-l-5-c-1", 5, 1));
+            risks.add(new Risk("", "Id-l-5-c-5", 5, 5));
+            risks.add(new Risk("", "Id-l-1-c-1", 1, 1));
+            risks.add(new Risk("", "Id-l-1-c-5", 1, 5));
+            risks.add(new Risk("", "Id-l-3-c-2", 3, 2));
+            risks.add(new Risk("", "Id-l-4-c-1", 4, 1));
+        }
 
         return risks;
     }
